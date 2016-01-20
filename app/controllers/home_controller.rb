24cf48997f3780137c26f4c4b4a308e59001b859
missahng <awr0607@gmail.com>
2016-01-20 05:24:55 +0000

    email ok

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,21 @@
 class HomeController < ApplicationController
     
     def index
+        render layout:false
     end
     
     def main
+        render layout:false
     end
     
-    
+    def send_email
+        name = params[:user_name]
+        major = params[:user_major]
+        email = params[:user_email]
+        phone = params[:user_phone]
+        message = params[:user_message]
+        
+        Email.welcome_email(name,major,email,phone,message).deliver_now
+        redirect_to '/home/main'
+    end
 end
