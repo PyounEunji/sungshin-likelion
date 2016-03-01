f65b30c42aa9f16e0c37f8ec07f5b319637fd654
편은지 <shine7765@naver.com>
2016-03-01 06:31:57 +0000

    sungshin@likelion.net

--- app/mailers/email.rb
+++ app/mailers/email.rb
@@ -1,7 +1,7 @@
 class Email < ApplicationMailer
     def welcome_email name, major, email, phone, message
         mail from: email, 
-        to: 'likelion.sungshin@gmail.com', 
+        to: 'sungshin@likelion.net', 
         subject: "[문의] #{name} / #{major}",
         body: "이메일: #{email}, 연락처: #{phone} \n메세지: #{message}"
     end
